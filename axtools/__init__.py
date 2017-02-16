7ea9d1789a554fae01aa96a9a4c9ca12ab65b9fd
Alex Xiao <alex@alexxiao.me>
2017-02-16 04:19:00 +0000

    updates

--- axtools/__init__.py
+++ axtools/__init__.py
@@ -4,17 +4,22 @@ The package of Alex's tools
 Should be placed at <jupyter notebook root>/axtoools
 
 __author__ = "Alex Xiao <http://www.alexxiao.me/>"
-__date__ = "2016-02-08"
+__date__ = "2017-02-08"
 __version__ = "0.5"
 
 """
 import sys,os,IPython
+
 platform=sys.platform
 jupyter=IPython.get_ipython()
 sys.path.append(os.path.join(jupyter.home_dir,'axtools'))
 #from DB import DB
 
 from import_from_file import load_module
-DB=load_module('axtools','DB')
-
+tools=load_module('axtools','tools')
+DB_module=load_module('axtools','DB')
+DB= DB_module.DB
+DB_Column=DB_module.Column
+cache=DB_module.im_memory_share
 from notebook_extract import JupyterNotebookExtract
+
